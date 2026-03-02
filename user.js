595622f39104495b51072a795ec1c7ccf49b43ab
MaxGiuP <max.g.pla@gmail.com>
2026-03-02 19:54:13 +0000

    Complete lightbird theme

--- user.js
+++ user.js
@@ -5,4 +5,5 @@
 
 /* --- enable userchrome theming --- */
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("svg.context-properties.content.enabled", true);
+/*user_pref("svg.context-properties.content.enabled", true);*/
+user_pref("mail.dark-reader.enabled", false);
